48bad79667252c02b44f19f51518999953aa55c2
MRilyes <ilyesghlila1@gmail.com>
2022-03-15 18:00:24 +0100

    creating models

---
+++ SocialMediaManagement/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaManagement.Models
+{
+    public class Group
+    {
+        [Key]
+        public int GroupID { get; set; }
+        public string GroupName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+    }
+}
---
+++ SocialMediaManagement/Models/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaManagement.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public string URL { get; set; }
+        public int Visible { get; set; }
+        public string Icon { get; set; }
+        public virtual ICollection<MenuItemAction> MenuItemActions { get; set; }
+    }
+}
