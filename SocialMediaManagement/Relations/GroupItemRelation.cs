2d3aaa09c5e80e9216bdadbe3f75dabd0e614ed1
MRilyes <ilyesghlila1@gmail.com>
2022-03-15 19:34:49 +0100

    manytomany relations

---
+++ SocialMediaManagement/Relations/GroupItemRelation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaManagement.Models
+{
+    public class GroupItemRelation
+    {
+        [Key]
+        public int GroupMenuItemRelationId { get; set; }
+
+        public int GroupID { get; set; }
+
+        [ForeignKey(nameof(GroupID))]
+
+        public virtual Group Group { get; set; }
+
+
+        public int ItemID { get; set; }
+
+        [ForeignKey(nameof(ItemID))]
+
+        public virtual MenuItem MenuItem { get; set; }
+
+
+
+    }
+}
