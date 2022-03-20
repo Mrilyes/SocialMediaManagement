6fe0c74d31ad56806a2d07142c2cd0e652c4ebc8
MRilyes <ilyesghlila1@gmail.com>
2022-03-20 18:51:34 +0100

    user management

---
+++ SocialMediaManagement/Controllers/dashboard/UserManagementController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMediaManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaManagement.Controllers.dashboard
+{
+    public class UserManagementController : Controller
+    {
+
+        private readonly UserManager<User> _userManager;
+
+        public UserManagementController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+      
+    }
+}
